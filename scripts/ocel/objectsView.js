7eb31a9a9914d10642bf9bcac09887acbc098cf9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-06-18 10:19:33 +0200

    issue in ordering
    issue in ordering

--- scripts/ocel/objectsView.js
+++ scripts/ocel/objectsView.js
@@ -20,16 +20,16 @@ class ObjectsView {
 			for (let evId in this.objectsIds[objId]) {
 				let eve = this.ocel["ocel:events"][evId];
 				this.objectsIdsSorted[objId].push([evId, eve["ocel:activity"], new Date(eve["ocel:timestamp"]).getTime()/1000.0]);
-				this.objectsIdsSorted[objId].sort(function(a, b) {
-						if (a[2] < b[2]) {
-							return -1;
-						}
-						else if (a[2] > b[2]) {
-							return 1
-						}
-						return 0;
-				});
 			}
+			this.objectsIdsSorted[objId].sort(function(a, b) {
+					if (a[2] < b[2]) {
+						return -1;
+					}
+					else if (a[2] > b[2]) {
+						return 1
+					}
+					return 0;
+			});
 		}
 	}
 }
