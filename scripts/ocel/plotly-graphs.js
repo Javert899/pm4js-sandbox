6ef31a8b28973b8e4290e2410705cbb63ead6ae3
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-06-28 11:15:44 +0200

    plotly graphs

--- scripts/ocel/plotly-graphs.js
+++ scripts/ocel/plotly-graphs.js
@@ -27,4 +27,17 @@ class PlotlyOcelGraphs {
 
 		Plotly.newPlot('plotlyOtGraph', data, layout);
 	}
+	
+	buildActivitiesGraph() {
+		let x = [];
+		let y = [];
+		for (let act in this.model.overallEventsView.activities) {
+			let actView = this.model.overallEventsView.activities[act];
+			x.push(act);
+			y.push(Object.keys(actView).length);
+		}
+		var data = [{x: x, y: y, type: 'bar'}];
+		var layout = {title: "Events per Activity", width: 1200, height: 400};
+		Plotly.newPlot('plotlyActGraph', data, layout);
+	}
 }
