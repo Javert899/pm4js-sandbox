da4aa30d6cdad4062b2610653aa2def6b7230e5e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-08-17 15:57:41 +0200

    added conformance checking on the lifecycle of the objects by means of using the log skeleton

---
+++ scripts/ocel/ocel_conf_lifecycle_objects.js
@@ -0,0 +1,65 @@
+class OcelConfLifecycleObjects {
+	constructor(ocel) {
+		this.ocel = ocel;
+		this.flattenedLogs = {};
+		this.skeletonModels = {};
+		this.skeletonConfResults = {};
+	}
+	
+	calculate(noiseThreshold=0.05) {
+		let objTypes = this.ocel["ocel:global-log"]["ocel:object-types"];
+		for (let objType of objTypes) {
+			let eventLog = OcelFlattening.flatten(this.ocel, objType);
+			this.flattenedLogs[objType] = eventLog;
+			let skeleton = LogSkeletonDiscovery.apply(eventLog, "concept:name");
+			this.skeletonModels[objType] = skeleton;
+			let skeletonConfResult = LogSkeletonConformanceChecking.apply(eventLog, skeleton, noiseThreshold).deviationsRecord;
+			this.skeletonConfResults[objType] = skeletonConfResult;
+		}
+	}
+	
+	humanReadableRule(rule) {
+		let ruleElements = rule.split(",");
+		let ret = rule;
+		if (ruleElements[0] == "alwaysBefore") {
+			ret = "<b>"+ruleElements[1]+"</b> is not<br />preceded by <b>"+ruleElements[2]+"</b>";
+		}
+		else if (ruleElements[0] == "alwaysAfter") {
+			ret = "<b>"+ruleElements[1]+"</b> is not<br />followed by <b>"+ruleElements[2]+"</b>";
+		}
+		else if (ruleElements[0] == "directlyFollows") {
+			/*console.log(rule);
+			ret = "<b>"+ruleElements[1]+"</b> is directly<br />followed by <b>"+ruleElements[2]+"</b>";
+			console.log(ret);*/
+		}
+		else if (ruleElements[0] == "actCounter") {
+			ret = "Unusual number of occurrences of the activity<br /><b>"+ruleElements[1]+"</b> ("+ruleElements[2]+")";
+		}
+		else if (ruleElements[0] == "equivalence") {
+			ret = "<b>"+ruleElements[1]+"</b> does not occur the same<br />number of times than <b>"+ruleElements[2]+"</b>";
+		}
+		else if (ruleElements[0] == "neverTogether") {
+			ret = "<b>"+ruleElements[1]+"</b> occurs together<br />with <b>"+ruleElements[2]+"</b>";
+		}
+		return ret;
+	}
+	
+	formTable(container) {
+		let content = [];
+		content.push("<thead><tr><th>Object Type</th><th>Rule</th><th>Violations</th><th>Filter</th></tr></thead>");
+		content.push("<tbody>");
+		for (let objType in this.skeletonConfResults) {
+			for (let dev in this.skeletonConfResults[objType]) {
+				content.push("<tr>");
+				content.push("<td>"+objType+"</td>");
+				content.push("<td>"+this.humanReadableRule(dev)+"</td>");
+				content.push("<td>"+this.skeletonConfResults[objType][dev].length+"</td>");
+				content.push("<td></td>");
+				content.push("</tr>");
+			}
+		}
+		content.push("</tbody>");
+		content = content.join("");
+		console.log(content);
+	}
+}
