4e74abde6c6cfaab83ea2550f4e2248d7e6d66fd
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-10-27 08:59:09 +0200

    celonis download and connection

---
+++ scripts/ocel/celonis_connection.js
@@ -0,0 +1,30 @@
+function connectToCelonis() {
+	document.getElementById("celonis_connection_parameters").style.display = "none";
+	document.getElementById("celonis_waiting_circle").style.display = "";
+	try {
+		celonisUrl = document.getElementById("celonis_url").value;
+		celonisApi = document.getElementById("celonis_api").value;
+		celonisMapper = new CelonisMapper(celonisUrl, celonisApi);
+		fillDataModelSelectionUpload();
+	}
+	catch (err) {
+		document.getElementById("celonis_connection_parameters").style.display = "";
+		document.getElementById("celonis_waiting_circle").style.display = "none";
+		console.log(err);
+	}
+}
+
+function fillDataModelSelectionUpload() {
+	let dataModelSelectionUpload = document.getElementById("dataModelSelectionUpload");
+	for (let analysisId in celonisMapper.analysis) {
+		let analysis = celonisMapper.analysis[analysisId];
+		let dataModelId = celonisMapper.analysisDataModel[analysisId];
+		let dataModel = celonisMapper.dataModels[dataModelId];
+		let dataPoolId = celonisMapper.dataModelsDataPools[dataModelId];
+		let dataPool = celonisMapper.dataPools[dataPoolId];
+		let option = document.createElement("option");
+		option.value = analysisId;
+		option.text = analysis["name"] + " (DP="+dataPool["name"]+"; DM="+dataModel["name"]+")";
+		dataModelSelectionUpload.appendChild(option);
+	}
+}
