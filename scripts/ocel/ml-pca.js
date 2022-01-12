a99e3ae35f518e5692241f4619285de754d6a978
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-12 13:48:41 +0100

    added colors to object types in dimensionality reduction graph

--- scripts/ocel/ml-pca.js
+++ scripts/ocel/ml-pca.js
@@ -1,32 +1,48 @@
 function applyPca(targetDiv="plotlyPCA") {
 		//let pca = new ML.PCA(objFeaturesNormalizedFiltered["data"]);
 		//let proj = pca.predict(objFeaturesNormalizedFiltered["data"]).data;
+		let dimRedTechnique = document.getElementById("dimensionalityReductionTechnique").value;
 		let matrix = druid.Matrix.from(objFeaturesNormalizedFiltered["data"]);
-		let proj = new druid.PCA(matrix, 2).transform().to2dArray;
-		console.log(proj);
-		console.log("BBBBBBBBBB");
+		let proj = null;
+		if (dimRedTechnique == "FASTMAP") {
+			proj = new druid.FASTMAP(matrix, 2).transform().to2dArray;
+		}
+		else if (dimRedTechnique == "PCA") {
+			proj = new druid.PCA(matrix, 2).transform().to2dArray;
+		}
+		else if (dimRedTechnique == "TSNE") {
+			proj = new druid.TSNE(matrix, 2).transform().to2dArray;
+		}
 		let objects = Object.keys(visualization.model.ocel["ocel:objects"]);
 		let objType = {};
+		let objTypes = {};
 		for (let objId of objects) {
 			let obj = visualization.model.ocel["ocel:objects"][objId];
 			objType[objId] = obj["ocel:type"];
+			objTypes[obj["ocel:type"]] = 0;
 		}
-		let serie = {x: [], y: [], text: [], type: "scatter", mode: "markers", name: "objects_PCA"};
-		let i = 0;
-		while (i < proj.length) {
-			serie.x.push(proj[i][0]);
-			serie.y.push(proj[i][1]);
-			serie.text.push(objects[i]+" ("+objType[objects[i]]+")");
-			i++;
+		objTypes = Object.keys(objTypes);
+		var data = [];
+		for (let ott of objTypes) {
+			let serie = {x: [], y: [], text: [], type: "scatter", mode: "markers", name: ott};
+			let i = 0;
+			while (i < proj.length) {
+				if (objType[objects[i]] == ott) {
+					serie.x.push(proj[i][0]);
+					serie.y.push(proj[i][1]);
+					serie.text.push(objects[i]+" ("+objType[objects[i]]+")");
+				}
+				i++;
+			}
+			data.push(serie);
 		}
-		var data = [serie];
 		var layout = {
-			title: "Objects PCA",
+			title: "Dimensionality Reduction",
 			xaxis: {
-				title: "First Principal Component"
+				title: "First Component"
 			},
 			yaxis: {
-				title: "Second Principal Component"
+				title: "Second Component"
 			}
 		};
 		Plotly.newPlot('plotlyPCA', data, layout, {responsive: true});
