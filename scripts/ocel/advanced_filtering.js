5bf9300ff2d6a5d70213bba02da8d4d63f0fca25
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-27 11:08:40 +0100

    added more meaningful description

--- scripts/ocel/advanced_filtering.js
+++ scripts/ocel/advanced_filtering.js
@@ -19,3 +19,35 @@ function filterEssentialEvents() {
 	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Essential Events&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterEssentialEvents0()");
 }
 
+function filterEssentialEventsOrMinOccurrencesActivity0() {
+	let minEvents = parseInt(prompt("Insert the minimum number of occurrences for an activity to be kept"));
+	let filteredModel = new OcdfgModel(OcelGeneralFiltering.filterEssentialEventsOrMinActCount(visualization.model.ocel, minEvents));
+	visualization.setFilteredModel(filteredModel);
+}
+
+function filterEssentialEventsOrMinOccurrencesActivity() {
+	showProcessModelPage();
+	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Essential Events OR Min Occurrences Activity&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterEssentialEventsOrMinOccurrencesActivity0()");
+}
+
+function filterOtMinNumRelatedEvents0() {
+	let minRelObj = parseInt(prompt("Insert the minimum number of events having relations to a specific object type, in order to keep the object type"));
+	let filteredModel = new OcdfgModel(OcelGeneralFiltering.filterOtMinNumRelatedEvents(visualization.model.ocel, minRelObj));
+	visualization.setFilteredModel(filteredModel);
+}
+
+function filterOtMinNumRelatedEvents() {
+	showProcessModelPage();
+	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Min Events per OT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterOtMinNumRelatedEvents0()");
+}
+
+function filterRateUniqueActivities0() {
+	let minRate = parseFloat(prompt("Insert the minimum rate of unique activities per objects of the given object type"));
+	let filteredModel = new OcdfgModel(OcelGeneralFiltering.filterOtRateUniqueActivities(visualization.model.ocel, minRate));
+	visualization.setFilteredModel(filteredModel);
+}
+
+function filterRateUniqueActivities() {
+	showProcessModelPage();
+	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Unique Activities&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterRateUniqueActivities0()");
+}
