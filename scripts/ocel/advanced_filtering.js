258d94650cab624f835972a6c29e3c1128d6b616
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-01-27 10:30:19 +0100

    added advanced filtering

--- scripts/ocel/advanced_filtering.js
+++ scripts/ocel/advanced_filtering.js
@@ -1,4 +1,21 @@
-function filterOtMinOccurrencesActivity() {
-	let minEvents = input("Insert the minimum number of occurrences for an activity to be kept");
-	
+function filterMinOccurrencesActivity0() {
+	let minEvents = parseInt(prompt("Insert the minimum number of occurrences for an activity to be kept"));
+	let filteredModel = new OcdfgModel(OcelGeneralFiltering.filterOtMinOccActivities(visualization.model.ocel, minEvents));
+	visualization.setFilteredModel(filteredModel);
 }
+
+function filterMinOccurrencesActivity() {
+	showProcessModelPage();
+	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Min Occurrences Activity&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterMinOccurrencesActivity0()");
+}
+
+function filterEssentialEvents0() {
+	let filteredModel = new OcdfgModel(OcelGeneralFiltering.filterEssentialEvents(visualization.model.ocel));
+	visualization.setFilteredModel(filteredModel);
+}
+
+function filterEssentialEvents() {
+	showProcessModelPage();
+	return addFilter("<i class=\"fas fa-user-minus\"></i>&nbsp;Essential Events&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "filterEssentialEvents0()");
+}
+
